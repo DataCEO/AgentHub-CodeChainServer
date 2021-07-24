f368a381f2cad48a47fde259ad8b2333c5df6a36
DataCEO <145150594+DataCEO@users.noreply.github.com>
2021-07-24 14:34:58 +0000

    commit 3

--- create_user_and_db.sql
+++ create_user_and_db.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE "codechain-agent-hub";
-CREATE USER "codechain-agent-hub" WITH ENCRYPTED PASSWORD
+CREATE USER "codechain-agent-hub" WITH ENCRYPTED PASSWORD 'preempt-entreat-bell-chanson';
+GRANT ALL PRIVILEGES ON DATABASE "codechai
