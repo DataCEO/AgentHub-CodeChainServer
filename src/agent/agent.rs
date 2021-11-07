e92890dc4d4a066780814258e6b8f65298e7cafb
DataCEO <145150594+DataCEO@users.noreply.github.com>
2021-11-07 19:43:38 +0000

    added settings

--- src/agent/agent.rs
+++ src/agent/agent.rs
@@ -259,4 +259,5 @@ impl Agent {
 
         let peers: Vec<SocketAddr> = self.codechain_rpc.get_peers(info.status)?;
         let best_block_id: Option<BlockId> = self.codechain_rpc.get_best_block_id(info.status)?;
+        let codechain_version = self.codechain_rpc.version(info.status)?;
         let c
