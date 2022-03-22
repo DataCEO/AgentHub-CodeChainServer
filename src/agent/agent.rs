cbb0427295e1a10c48242c10bb13b786d099287a
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-03-22 21:58:46 +0000

    Removed submodule

--- src/agent/agent.rs
+++ src/agent/agent.rs
@@ -372,4 +372,4 @@ pub trait SendAgentRPC {
     fn shell_get_codechain_log(&self) -> RPCResult<Vec<StructuredLog>>;
     fn agent_get_info(&self) -> RPCResult<AgentGetInfoResponse>;
     fn codechain_call_rpc_raw(&self, args: (String, Vec<Value>)) -> RPCResult<CodeChainCallRPCResponse>;
-    fn codec
+    fn codechain_call_rpc(&self, args: (String, Vec<Value>)) -> RPCResult<Output
