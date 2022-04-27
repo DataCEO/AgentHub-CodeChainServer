a35e5b37ae5c8e519d57e8bd842a2ca74afc63c5
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-04-27 11:49:29 +0000

    Reportes

--- src/agent/codechain_rpc.rs
+++ src/agent/codechain_rpc.rs
@@ -5,4 +5,5 @@ use serde::de::DeserializeOwned;
 use serde_json;
 use serde_json::Value;
 
-use super::super::common_rpc_types::{BlackList, BlockId, NodeStat
+use super::super::common_rpc_types::{BlackList, BlockId, NodeStatus, PendingParcel, StructuredLog, WhiteList};
+use super::agent::{Agen
