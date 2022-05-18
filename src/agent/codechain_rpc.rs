5c5c5f4d52c8ddcb2f12dff5d6923267ee15d482
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-05-18 19:34:21 +0000

    x86/efi: Include a .bss section within the PE/COFF headers

--- src/agent/codechain_rpc.rs
+++ src/agent/codechain_rpc.rs
@@ -47,4 +47,6 @@ impl CodeChainRPC {
         Ok(Vec::new())
     }
 
-    pub fn get_whitelist(&self, status: NodeStatus) -> Result<Option<WhiteList>, S
+    pub fn get_whitelist(&self, status: NodeStatus) -> Result<Option<WhiteList>, String> {
+        self.call_rpc(status, "net_getWhitelist", Vec::new())
+    
