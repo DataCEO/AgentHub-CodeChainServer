2d69e94cd8b6f0635d9ebb51ae0d2418da6bd50e
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-08-25 10:37:48 +0000

    License and README

--- src/agent/types.rs
+++ src/agent/types.rs
@@ -3,4 +3,6 @@ use std::net::SocketAddr;
 use cprimitives::H256;
 use serde_json::Value;
 
-use super::super::
+use super::super::common_rpc_types::{NodeName, NodeStatus};
+
+#[derive(Debug, Seriali
