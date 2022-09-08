4eb33cec8a3f566fdf07d7284155e8a7d9d581e1
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-09-08 10:35:49 +0000

    Primera pregunta

--- src/bin/delete-all-table.rs
+++ src/bin/delete-all-table.rs
@@ -3,3 +3,9 @@ extern crate codechain_agent_hub as chub;
 extern crate postgres;
 #[macro_use]
 extern crate log;
+
+use chub::logger_init;
+use postgres::{Connection, TlsMode};
+
+fn main() {
+    l
