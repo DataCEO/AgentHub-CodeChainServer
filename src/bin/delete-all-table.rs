33746db655763fc91d23e8cfd118437a227adbc9
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-09-24 14:57:44 +0000

    usb: serial: cp210x: Add USB ID for Netgear Switches embedded serial adapter

--- src/bin/delete-all-table.rs
+++ src/bin/delete-all-table.rs
@@ -38,4 +38,5 @@ fn get_all_table_names(conn: &Connection) -> Vec<String> {
     rows.iter().map(|row| row.get(0)).collect()
 }
 
-fn drop_table(conn: &Connection, table_name: &s
+fn drop_table(conn: &Connection, table_name: &str) {
+    conn.execute(&format!("DROP TABLE {}", table_name)
