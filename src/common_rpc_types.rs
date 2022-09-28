0e710d05abb44d1f9f75642214301290e0bd1462
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-09-28 06:11:28 +0000

    Folder restructure

--- src/common_rpc_types.rs
+++ src/common_rpc_types.rs
@@ -49,3 +49,56 @@ pub struct NodeVersion {
 }
 
 pub type PendingParcel = serde_json::Value;
+
+pub type Tag = String;
+
+#[derive(Debug, Serialize, PartialEq, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WhiteList {
+    pub list: Vec<(IpAddr, Tag)>,
+    pub enabled: bool,
+}
+
+pub type BlackList = WhiteList;
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct HardwareUsage {
+    pub total: i64,
+    pub available: i64,
+    pub percentage_used: f64,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct HardwareInfo {
+    pub cpu_usage: Vec<f64>,
+    pub disk_usage: HardwareUsage,
+    pub memory_usage: HardwareUsage,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct StructuredLog {
+    pub level: String,
+    pub target: String,
+    pub message: String,
+    pub timestamp: String,
+    pub thread_name: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(tag = "type")]
+pub enum UpdateCodeChainRequest {
+    #[serde(rename_all = "camelCase")]
+    Git {
+        commit_hash: String,
+    },
+    #[serde(rename_all = "camelCase")]
+    Binary {
+        #[serde(rename = "binaryURL")]
+        binary_url: String,
+        binary_checksum: String,
+    },
+}
