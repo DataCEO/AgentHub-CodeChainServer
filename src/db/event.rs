8df053f336c6ba3682eb05be1bc9678c79996482
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-09-30 09:28:27 +0000

    modified README file

--- src/db/event.rs
+++ src/db/event.rs
@@ -4,4 +4,7 @@ use super::types::{AgentExtra, AgentQueryResult};
 pub enum Event {
     AgentUpdated {
         before: Box<Option<AgentQueryResult>>,
-    
+        after: Box<AgentQueryResult>,
+    },
+    ConnectionChanged {
+        a
