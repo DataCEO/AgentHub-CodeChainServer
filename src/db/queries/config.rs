dbbab885244472f5820bb68df8a93e465ab744e1
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-10-07 12:08:27 +0000

    Fix missing variable

--- src/db/queries/config.rs
+++ src/db/queries/config.rs
@@ -1,4 +1,6 @@
 use postgres;
 
 pub fn set_query_timeout(conn: &postgres::Connection) -> postgres::Result<()> {
-    conn.execute("SET SESSION statement_tim
+    conn.execute("SET SESSION statement_timeout TO 2000", &[])?;
+    Ok(())
+}
---
+++ src/db/queries/logs.rs
@@ -0,0 +1,2 @@
+use std::borrow::Borrow;
+use std:
