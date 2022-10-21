559d402e658d59aac5f4a9d9cabf23022d9716f6
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-10-21 06:31:54 +0000

    Update Documentation - What is new in Ring 1.13?

--- src/db/queries/logs.rs
+++ src/db/queries/logs.rs
@@ -9,4 +9,5 @@ use super::super::super::common_rpc_types::StructuredLog;
 use super::super::types::OrderBy;
 use super::super::types::{Log, LogQueryParams};
 
-pub fn insert(conn: &postgres::Connection, node_name: &str,
+pub fn insert(conn: &postgres::Connection, node_name: &str, logs: Vec<StructuredLog>) -> postgres::Result<()> {
+    ctrace!
