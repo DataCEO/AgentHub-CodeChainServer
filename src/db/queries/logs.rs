13ba9502d9abb4221dddba131a72c74ff0ac7f84
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-12-05 05:39:13 +0000

    mnt: Move the test for MNT_LOCK_READONLY from change_mount_flags into do_remount

--- src/db/queries/logs.rs
+++ src/db/queries/logs.rs
@@ -79,4 +79,5 @@ pub fn search(conn: &postgres::Connection, params: LogQueryParams) -> postgres::
     }
     if let Some(search) = params.search {
         if search != "" {
-            let search_index = paramete
+            let search_index = parameters.add(Rc::new(format!("%{}%", search)));
+            wher
