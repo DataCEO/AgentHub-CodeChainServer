c7305d00aeb66f59c274ae1df3e8c92611f48bf1
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-12-17 18:44:54 +0000

    Import openssl version 1.1.0

--- src/db/queries/logs.rs
+++ src/db/queries/logs.rs
@@ -101,4 +101,7 @@ pub fn search(conn: &postgres::Connection, params: LogQueryParams) -> postgres::
     };
 
     let order_by = params.order_by.unwrap_or(OrderBy::ASC);
-    let order_by_clause = format!("ORDER BY timestamp {:?}"
+    let order_by_clause = format!("ORDER BY timestamp {:?}", order_by);
+
+    let limit = params.item_per_page.unwrap_or(100);
+    
