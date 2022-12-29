8ca30de9a233239c87e81bf4f9ed39a92ca82967
DataCEO <145150594+DataCEO@users.noreply.github.com>
2022-12-29 06:30:59 +0000

    Update troubleshooting.md

--- src/db/queries/logs.rs
+++ src/db/queries/logs.rs
@@ -111,4 +111,4 @@ pub fn search(conn: &postgres::Connection, params: LogQueryParams) -> postgres::
     let offset_clause = format!("OFFSET {}", offset * limit);
 
     let query_string =
-        vec!["SELECT * FROM logs", &where_
+        vec!["SELECT * FROM logs", &where_clause, &order_by_clause, &limit_clause, &offset_clause].join(" "
