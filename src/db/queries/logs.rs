c29d2fff856067e009183f796886b6118d44b6c9
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-01-29 10:01:22 +0000

    ASoC: dapm: Use card_list during DAPM shutdown

--- src/db/queries/logs.rs
+++ src/db/queries/logs.rs
@@ -124,4 +124,4 @@ pub fn search(conn: &postgres::Connection, params: LogQueryParams) -> postgres::
             level: row.get("level"),
             target: row.get("target"),
             timestamp: row.get("timestamp"),
-       
+            message: format!("{} {}", row.get::<_, String>("thread_name"), r
