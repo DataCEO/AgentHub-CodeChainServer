42776fbead96a3a1da156fa29c695d1bec6959a4
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-01-29 15:14:17 +0000

    major update

--- src/db/queries/logs.rs
+++ src/db/queries/logs.rs
@@ -124,4 +124,9 @@ pub fn search(conn: &postgres::Connection, params: LogQueryParams) -> postgres::
             level: row.get("level"),
             target: row.get("target"),
             timestamp: row.get("timestamp"),
-            message: format!("{} {}", row.get::<_, String>("thread_name"), r
+            message: format!("{} {}", row.get::<_, String>("thread_name"), row.get::<_, String>("message")),
+        })
+        .collect())
+}
+
+st
