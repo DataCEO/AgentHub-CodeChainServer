ccefa328271c0f342d6992e7091832857fbbef2f
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-03-01 08:29:53 +0000

    refactoring persistence

--- src/db/service.rs
+++ src/db/service.rs
@@ -23,3 +23,53 @@ pub enum Message {
     GetAgent(NodeName, Sender<Option<AgentQueryResult>>),
     GetAgents(Sender<Vec<AgentQueryResult>>),
     GetConnections(Sender<Vec<rpc_type::Connection>>),
+    SaveStartOption(NodeName, String, String),
+    GetAgentExtra(NodeName, Sender<Option<AgentExtra>>),
+    GetLogs(LogQueryParams, Sender<Vec<Log>>),
+    WriteLogs(NodeName, Vec<StructuredLog>),
+    GetLogTargets(Sender<Vec<String>>),
+}
+
+#[derive(Clone)]
+pub struct ServiceSender {
+    sender: Sender<Message>,
+}
+
+struct State {
+    agent_query_result: HashMap<NodeName, AgentQueryResult>,
+    connection: Connections,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            agent_query_result: HashMap::new(),
+            connection: Connections::new(),
+        }
+    }
+}
+
+pub struct Service {
+    state: State,
+    event_subscriber: Box<EventSubscriber>,
+    db_conn: postgres::Connection,
+}
+
+pub struct ServiceNewArg {
+    pub event_subscriber: Box<EventSubscriber>,
+    pub db_user: String,
+    pub db_password: String,
+}
+
+impl Service {
+    fn new(
+        ServiceNewArg {
+            event_subscriber,
+            db_user,
+            db_password,
+        }: ServiceNewArg,
+    ) -> Self {
+        let conn_uri = format!("postgres://{}:{}@localhost", db_user, db_password);
+
+        let conn = postgres::Connection::connect(conn_uri, TlsMode::None).unwrap();
+        queries::config::set_query_timeout(&conn).unwrap();
