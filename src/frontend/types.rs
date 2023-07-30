8b0c334355c6a40a118927ca61a51850ad23ce6d
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-07-30 09:35:56 +0000

    Update videos.md

--- src/frontend/types.rs
+++ src/frontend/types.rs
@@ -3,4 +3,6 @@ use std::net::SocketAddr;
 use super::super::agent;
 use super::super::common_rpc_types;
 use super::super::common_rpc_types::{
-    BlackList, BlockId, HardwareInfo, HardwareUsage, NodeName, NodeStatus,
+    BlackList, BlockId, HardwareInfo, HardwareUsage, NodeName, NodeStatus, NodeVersion, PendingParcel, WhiteList,
+};
+use super::super::db;
