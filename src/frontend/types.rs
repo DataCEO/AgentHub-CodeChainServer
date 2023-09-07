4f69874d8790bdf7f8bf923a023e92aca32c708d
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-09-07 18:48:24 +0000

    merge from default

--- src/frontend/types.rs
+++ src/frontend/types.rs
@@ -104,4 +104,6 @@ impl NodeGetInfoResponse {
             address: Some("127.0.0.1:3485".parse().unwrap()),
             version: Some(NodeVersion {
                 version: "0.1.0".to_string(),
-                hash: "d6fb3195876b6b175902d25dd621db9952
+                hash: "d6fb3195876b6b175902d25dd621db99527ccb6f".to_string(),
+                binary_checksum: "".to_string(),
+    
