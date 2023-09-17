7a49daaac72d6dd12121118674b833593c09a03e
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-09-17 12:52:30 +0000

    Create 6.html

--- src/frontend/types.rs
+++ src/frontend/types.rs
@@ -132,4 +132,5 @@ impl NodeGetInfoResponse {
                 },
                 memory_usage: HardwareUsage {
                     total: 8 * 1000 * 1000 * 1000,
-                    available: 5 * 1000 *
+                    available: 5 * 1000 * 1000 * 1000,
+                    percentage_used:
