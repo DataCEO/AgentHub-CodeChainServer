f913497d7a860e6a0bba43f186cce1991e8aa111
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-10-07 08:48:47 +0000

    Auto-merge from mysql-next-mr-bugfixing.

--- src/jsonrpc.rs
+++ src/jsonrpc.rs
@@ -24,3 +24,68 @@ use super::router::Error as RouterError;
 use super::ws::{Error as WSError, Message, Sender as WSSender};
 
 pub fn handle<F>(router: F, text: String) -> Option<String>
+where
+    F: FnOnce(String, Value) -> Result<Option<Value>, RouterError>, {
+    let deserialized = serde_json::from_str(&text);
+    let response: Option<Response> = match deserialized {
+        Err(_) => Some(
+            Failure {
+                jsonrpc: None,
+                id: Id::Null,
+                error: JSONRPCError::new(ErrorCode::ParseError),
+            }
+            .into(),
+        ),
+        Ok(Call::Invalid(id)) => Some(
+            Failure {
+                jsonrpc: None,
+                id,
+                error: JSONRPCError::new(ErrorCode::ParseError),
+            }
+            .into(),
+        ),
+        Ok(Call::MethodCall(MethodCall {
+            id,
+            method,
+            params,
+            ..
+        })) => {
+            let value_params = serde_json::to_value(params.clone()).expect("Change to value always success");
+            match router(method.clone(), value_params) {
+                Ok(Some(value)) => Some(
+                    Success {
+                        jsonrpc: None,
+                        result: value,
+                        id,
+                    }
+                    .into(),
+                ),
+                Ok(None) => {
+                    let mut error = JSONRPCError::new(ErrorCode::InternalError);
+                    error.data = Some(serde_json::Value::String("API returns no value".to_string()));
+                    Some(
+                        Failure {
+                            jsonrpc: None,
+                            id,
+                            error,
+                        }
+                        .into(),
+                    )
+                }
+                Err(RouterError::MethodNotFound) => Some(
+                    Failure {
+                        jsonrpc: None,
+                        id,
+                        error: JSONRPCError::new(ErrorCode::MethodNotFound),
+                    }
+                    .into(),
+                ),
+                Err(RouterError::RPC(err)) => {
+                    cwarn!("Error while handlinig {}({:#?}) : {}", method, params, err);
+                    Some(
+                        Failure {
+                            jsonrpc: None,
+                            id,
+                            error: err.to_jsonrpc_error(),
+                        }
+                        .into(),
