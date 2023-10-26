2868cd00bdfd90a8520e77cfa4ceceffc8de8704
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-10-26 22:28:19 +0000

    Update squadbuilderjapan.html

--- src/logger/logger.rs
+++ src/logger/logger.rs
@@ -54,4 +54,5 @@ impl Log for Logger {
             };
             let log_level = record.level();
             let log_target = record.target();
-            
+            let log_message = record.args();
+            eprintln!("#{} {
