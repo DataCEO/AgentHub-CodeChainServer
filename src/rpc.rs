5ae0937becfdf2f879059e8f8d3264aac3880a3e
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-11-24 08:56:17 +0000

    chore(ci): update actions/setup-node action to v2.4.0

--- src/rpc.rs
+++ src/rpc.rs
@@ -2,4 +2,5 @@ use std::error::Error;
 use std::fmt;
 use std::result::Result;
 
-use jsonrpc_core::types::{Error
+use jsonrpc_core::types::{Error as JSONRPCError, ErrorCode};
+use serde_json::{Error as SerdeError,
