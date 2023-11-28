fe119cca2f09350fe169ea5b6652d5814aed9e3c
DataCEO <145150594+DataCEO@users.noreply.github.com>
2023-11-28 19:06:27 +0000

    v0.9.5

--- src/rpc.rs
+++ src/rpc.rs
@@ -24,4 +24,4 @@ impl fmt::Display for RPCError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             RPCError::Internal(err) => write!(f, "RPCError {}", err),
-        
+            RPCError::FromAgent(err) => write!(f, "JSONRPCError 
